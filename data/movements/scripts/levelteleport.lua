30bbcf6bf958d01173d09cc20b72cd996f6b2b99
Keivn <kevin@wehype.it>
2016-11-12 20:10:04 +0100

    Level teleport
    Need to be checked.

--- data/movements/scripts/levelteleport.lua
+++ data/movements/scripts/levelteleport.lua
@@ -1,10 +1,16 @@
-function onStepIn(cid, item, position, fromPosition)
-	pos1 = {x=1000, y=1001, z=7}  ------- Enter the position the teleport should take you here
-	pos2 = {x=1000, y=1002, z=7}  ------- Enter the position the teleport should take you if you're not level 100 here
-	if getPlayerLevel(cid) >= 100 then
-		doTeleportThing(cid, pos1, 1)
-	else
-		doTeleportThing(cid, pos2, 1)
-		return TRUE
+function onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	local level = item.actionid - 1000
+
+	if player == nil then
+		return false
 	end
+
+	if player:getLevel() < level then
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "Only the worthy may pass.")
+		player:teleportTo(fromPosition, true)
+		return false
+	end
+
+	return true
 end
