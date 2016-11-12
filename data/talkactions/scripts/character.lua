7dbd8d8fa50f44f0f5c039c914bb0ef528064e1e
Keivn <kevin@wehype.it>
2016-11-12 20:10:14 +0100

    Changed in !character
    Character

--- data/talkactions/scripts/character.lua
+++ data/talkactions/scripts/character.lua
@@ -1,9 +1,19 @@
 -- The helper lib is used by passing a table value to the ModalWindow function
-local window = ModalWindow {
-    title = 'Character Information',
-    message = 'test\ntest'
-}
+
+
 
 function onSay(player, words, param)
+	local name = db.query("SELECT 1 FROM players WHERE account_id = 1")
+
+	local window = ModalWindow {
+	    title = 'Character Information',
+	    message = '---- GENERAL INFORMATION ----\nName: ' .. player:getName() .. '\nLevel: ' .. player:getLevel()
+	}
+
+	window:addButton('Close')
+	window:setDefaultEscapeButton('Close')
+
 	window:sendToPlayer(player)
+
+	result.free(name)
 end
